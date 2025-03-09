0ee1dff43adbb4c9846696e0d32b170022a9f245
imaadzaffar <56759143+imaadzaffar@users.noreply.github.com>
2025-03-09 07:01:59 +0000

    add map markers for saved and visited

--- app/(tabs)/map.jsx
+++ app/(tabs)/map.jsx
@@ -2,19 +2,67 @@ import React, { useEffect, useState } from "react";
 import MapView, { Marker } from "react-native-maps";
 import { StyleSheet, View } from "react-native";
 
-import { getAllRestaurants } from "@/lib/firebase";
+import {
+  getAllRestaurants,
+  getUserSavedRestaurants,
+  getUserReviewedRestaurants,
+  getRestaurantsByIds,
+} from "@/lib/firebase";
 
 export default function MapScreen() {
   const [restaurants, setRestaurants] = useState([]);
+  // const [visited, setVisited] = useState([]);
+  // const [saved, setSaved] = useState([]);
 
   const fetchRestaurants = async () => {
-    const data = await getAllRestaurants();
-    // console.log(data);
-    setRestaurants(data);
-    // console.log(restaurants.length);
-    // restaurants.forEach((d) => {
-    //   console.log(d.coordinates);
-    // });
+    console.log("hello");
+    let restaurants = [];
+    const allRestaurants = await getAllRestaurants();
+    console.log(allRestaurants);
+    // setRestaurants(allRestaurants);
+    restaurants = [...allRestaurants];
+    console.log(restaurants.length);
+
+    const userId = "NK5hqbtMQvcTRISmZjQaVVhACBH2";
+
+    const savedIds = await getUserSavedRestaurants(userId);
+    const savedRestaurants = await getRestaurantsByIds(savedIds);
+    savedRestaurants.forEach((s) => {
+      const index = restaurants.findIndex((r) => s.id === r.id);
+      if (index === -1) restaurants.push({ type: "saved", ...s });
+      else restaurants[index].type = "saved";
+    });
+    // restaurants = restaurants.concat(
+    //   savedRestaurants.map((r) => {
+    //     return { type: "saved", ...r };
+    //   })
+    // );
+    console.log(restaurants.length);
+    // setSaved(savedRestaurants);
+
+    const reviewedIds = await getUserReviewedRestaurants(userId);
+    const visitedRestaurants = await getRestaurantsByIds(reviewedIds);
+    visitedRestaurants.forEach((v) => {
+      const index = restaurants.findIndex((r) => v.id === r.id);
+      if (index === -1) restaurants.push({ type: "visited", ...v });
+      else restaurants[index].type = "visited";
+    });
+    // restaurants = restaurants.concat(
+    //   visitedRestaurants.map((r) => {
+    //     return { type: "visited", ...r };
+    //   })
+    // );
+    console.log(restaurants.length);
+    // setVisited(visitedRestaurants);
+
+    // console.log(restaurants);
+    setRestaurants(restaurants);
+  };
+
+  const pinColors = {
+    all: "red",
+    saved: "yellow",
+    visited: "blue",
   };
 
   useEffect(() => {
@@ -25,6 +73,7 @@ export default function MapScreen() {
     <View style={styles.container}>
       <MapView
         key={restaurants.length}
+        // provider="google"
         style={styles.map}
         initialRegion={{
           latitude: 51.505,
@@ -32,18 +81,44 @@ export default function MapScreen() {
           latitudeDelta: 0.2,
           longitudeDelta: 0.2,
         }}
+        showsUserLocation={true}
       >
         {restaurants.map((r, index) => (
           <Marker
-            key={index}
+            key={r.id}
             coordinate={{
               latitude: r.coordinates.latitude,
               longitude: r.coordinates.longitude,
             }}
             title={`${r.name} (${r.google_rating})`}
             description={r.cuisines.join(",")}
+            pinColor={pinColors[r.type]}
           />
         ))}
+        {/* {saved.map((r, index) => (
+          <Marker
+            key={r.id}
+            coordinate={{
+              latitude: r.coordinates.latitude,
+              longitude: r.coordinates.longitude,
+            }}
+            title={`${r.name} (${r.google_rating})`}
+            description={r.cuisines.join(",")}
+            pinColor="yellow"
+          />
+        ))}
+        {visited.map((r, index) => (
+          <Marker
+            key={r.id}
+            coordinate={{
+              latitude: r.coordinates.latitude,
+              longitude: r.coordinates.longitude,
+            }}
+            title={`${r.name} (${r.google_rating})`}
+            description={r.cuisines.join(",")}
+            pinColor="blue"
+          />
+        ))} */}
       </MapView>
     </View>
   );
