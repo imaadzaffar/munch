fe29010a9e20c4dda0b122a0c2e666ffe72d7a05
Imaad Zaffar <56759143+imaadzaffar@users.noreply.github.com>
2025-03-16 17:45:49 +0000

    header

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -15,17 +15,15 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         // tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: false,
-        // tabBarActiveTintColor: "blue",
-        // tabBarButton: HapticTab,
-        // tabBarBackground: TabBarBackground,
-        // tabBarStyle: Platform.select({
-        //   ios: {
-        //     // Use a transparent background on iOS to show the blur effect
-        //     position: "absolute",
-        //   },
-        //   default: {},
-        // }),
+        // headerShown: false,
+        // headerTitle: "munch",
+        headerStyle: {
+          backgroundColor: "#3A6444",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
       }}
     >
       <Tabs.Screen
@@ -35,13 +33,6 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
-      {/* <Tabs.Screen
-        name="explore"
-        options={{
-          title: "Explore",
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      /> */}
       <Tabs.Screen
         name="explore"
         options={{
