5502d1c1b9634dba86de9de7295132c48d8fcf51
Imaad Zaffar <56759143+imaadzaffar@users.noreply.github.com>
2025-03-16 17:02:35 +0000

    load fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,14 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
 
+import { useFonts } from "expo-font";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { FontAwesome5, Ionicons } from "@expo/vector-icons";
+import { FontAwesome6 } from "@expo/vector-icons";
 import "./global.css";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -14,9 +16,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
+  const [loaded] = useFonts({ ...Ionicons.font, ...FontAwesome6.font });
 
   useEffect(() => {
     if (loaded) {
