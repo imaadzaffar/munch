b3c12032ab71ce9cb115d9df75500ffc979e8339
Imaad Zaffar <56759143+imaadzaffar@users.noreply.github.com>
2025-03-16 17:09:36 +0000

    cuisine emoji

--- components/ui/RestaurantMapMarker.tsx
+++ components/ui/RestaurantMapMarker.tsx
@@ -3,13 +3,16 @@ import { StyleSheet, Text, View } from "react-native";
 import { Restaurant } from "@/lib/types";
 import { ThemedText } from "@/components/ThemedText";
 import { FontAwesome6 } from "@expo/vector-icons";
+import { getCuisineEmoji } from "@/lib/utils";
 
 export default function RestaurantMapMarker({ restaurant }: { restaurant: Restaurant }) {
   return (
     <View className="flex-1 flex-row items-center rounded-full py-1 px-2 border-2 border-stone-400 gap-1 bg-white">
-      <FontAwesome6 name="utensils" size={12} color="orange" />
+      {/* <FontAwesome6 name="utensils" size={12} color="orange" /> */}
       <View>
-        <Text className="text-sm font-bold">{restaurant.google_rating.toFixed(1)}</Text>
+        <Text className="text-sm font-bold">
+          {getCuisineEmoji(restaurant.cuisines[0])} {restaurant.google_rating.toFixed(1)}
+        </Text>
       </View>
     </View>
   );
