a9b88f5450162d028b76845b9e09297d766fdfc5
Imaad Zaffar <56759143+imaadzaffar@users.noreply.github.com>
2025-03-17 00:49:17 +0000

    restaurant detail card

--- components/ui/RestaurantDetailCard.tsx
+++ components/ui/RestaurantDetailCard.tsx
@@ -11,6 +11,14 @@ cssInterop(Image, { className: "style" });
 cssInterop(ThemedText, { className: "style" });
 
 export default function RestaurantDetailCard({ restaurant }: { restaurant: Restaurant }) {
+  console.log(restaurant.halal_info.last_verified);
+
+  const timestampToDate = timestamp => {
+    const milliseconds = timestamp._seconds * 1000 + timestamp._nanoseconds / 1e6;
+    const date = new Date(milliseconds);
+    return date.toDateString();
+  };
+
   return (
     <View className="w-full p-4 border-b-2 border-white gap-4">
       <ThemedText className="text-3xl font-bold">{restaurant.name}</ThemedText>
@@ -28,15 +36,26 @@ export default function RestaurantDetailCard({ restaurant }: { restaurant: Resta
             </Chip>
           ))}
         </View>
-        <ThemedText className="text-sm">
+        <ThemedText className="mt-2 font-bold">
           {`⭐️ ${restaurant.google_rating} (${restaurant.google_rating_count})`} {`❤️ 0 friends`}
         </ThemedText>
       </View>
       <Image className="h-[150] w-[150] rounded-lg" source={restaurant.img} />
-      <HalalIcon size={24} strokeWidth={2} color={restaurant.halal_info.fully_halal ? "green" : "red"} />
-      <Button>Get directions</Button>
-      <Button>Save to wishlist</Button>
-      <Button>Add review</Button>
+      <View>
+        <HalalIcon size={36} strokeWidth={2} color={restaurant.halal_info.fully_halal ? "green" : "red"} />
+        <Text>Last verified: {timestampToDate(restaurant.halal_info.last_verified)}</Text>
+      </View>
+      <View className="flex-row gap-2">
+        <Button mode="contained-tonal" onPress={() => alert("Get directions")}>
+          Directions
+        </Button>
+        <Button mode="contained-tonal" onPress={() => alert("Save to wishlist")}>
+          Save
+        </Button>
+        <Button mode="contained-tonal" onPress={() => alert("Add review")}>
+          Review
+        </Button>
+      </View>
     </View>
   );
 }
